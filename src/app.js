b9717ef5b6ed567ca90b013f87545a07169106ac
DannySuarez <suarezd10@gmail.com>
2019-04-25 14:24:22 -0700

    added event listeners to ball buttons and logged them

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-const
+const ballButtons = document.querySelectorAll('.ball-button');
+for(let i = 0; i < ballButtons.length; i++) {
+    const ballButton = ballButtons[i];
+    ballButton.addEventListener('click', () => {
+        console.log(ballButton.value);
+    });
+}
