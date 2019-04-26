76e831695979361e287c5e952e94c40c587cf07b
Dan Hogan <hogan.daniel@hotmail.com>
2019-04-26 09:33:20 -0700

    Added dance functions and also CCS rules for dance

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import addBall from './addBall.js';
-import makeDance from './makeDance.js';
 
 const ballButtons = document.querySelectorAll('.ball-button');
 const ballParent = document.getElementById('ball-parent');
@@ -11,13 +10,47 @@ for(let i = 0; i < ballButtons.length; i++) {
     });
 }
 
-const danceButtons = document.querySelectorAll('.dance-button'); 
+const danceButtons = document.querySelectorAll('.dance-button');
 
 for(let i = 0; i < danceButtons.length; i++) {
     const danceButton = danceButtons[i];
     danceButton.addEventListener('click', () => {
-        makeDance(danceButton.value);
-        console.log('')
+        removeDance();
+        let targetClass = determineClass(danceButton.value);
+        addClass(targetClass);
     });
 }
 
+function determineClass(danceButtonValue){
+    let classToSelect;
+
+    if(danceButtonValue === 'all') {
+        classToSelect = ('ball');
+    } else if(danceButtonValue === 'stripe') {
+        classToSelect = 'stripe';
+    } else if(danceButtonValue === 'solid') {
+        classToSelect = 'solid';
+    } else if(danceButtonValue === 'eight-ball') {
+        classToSelect = 'eight-ball';
+    }
+    return classToSelect;
+}
+
+function addClass(targetClass) {
+    const targetBalls = document.getElementsByClassName(targetClass);
+
+    for(let i = 0; i < targetBalls.length; i++) {
+        let ball = targetBalls[i];
+        ball.classList.add('dance');
+    }
+}
+
+function removeDance() {
+    const targetBalls = document.querySelectorAll('.dance');
+
+    for(let x = 0; x < targetBalls.length; x++) {
+        let ball = targetBalls[x];
+        ball.classList.remove('dance');
+    }
+
+}
